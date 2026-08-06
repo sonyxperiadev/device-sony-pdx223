f724c376f9f3102efa3f18bacab35a1256010ee1
Pavel Dubrova <pashadubrova@gmail.com>
2026-08-06 13:19:24 +0300

    device: Add vendor prefix to USB PID suffix property
    The property is vendor-specific and must use a vendor prefix.
    The USB Gadget HAL expects the updated property.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- device.mk
+++ device.mk
@@ -76,7 +76,7 @@ PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.sf.lcd_density=420 \
-    ro.usb.pid_suffix=20d
+    ro.vendor.usb.pid_suffix=20d
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/sony/nagara/platform.mk)
